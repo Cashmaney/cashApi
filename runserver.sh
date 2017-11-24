6796be9aca701f14bd55c181972dd23751c3d6cf
King <itzgro@gmail.com>
2017-11-24 10:01:04 -0800

    Okay, I think I got it....

--- runserver.sh
+++ runserver.sh
@@ -4,6 +4,6 @@ cd /var/app
 export PYTHONPATH=/var/app;$PYTHONPATH
 
 python manage.py migrate --noinput
-
+python manage.py collectstatic --noinput
 python manage.py initadmin
 python manage.py runserver 0.0.0.0:8080
