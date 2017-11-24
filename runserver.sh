e36f31f78c1c24ce4bc9420ac4c5bf412c0d7a1f
King <itzgro@gmail.com>
2017-11-24 09:35:26 -0800

    hm

--- runserver.sh
+++ runserver.sh
@@ -4,7 +4,7 @@ cd /var/app
 export PYTHONPATH=/var/app;$PYTHONPATH
 
 python manage.py migrate --noinput
-python manage.py collectstatic --noinput
+
 
 python manage.py initadmin
 python manage.py runserver 0.0.0.0:8080
